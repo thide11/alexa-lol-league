2b96f4b58920a1f6f8904e62a923aa3ebe73da00
THIAGO VIANA AVELAR DE SOUZA <thide2001@gmail.com>
2021-09-30 23:54:34 -0300

    Adicionado banco de dados verdadeiro 4° tentativa

--- database.js
+++ database.js
@@ -1,8 +1,11 @@
-const postgres = require("postgres");
+const { Client } = require("pg");
 
 module.exports = function() {
-  const sql = postgres(process.env.DATABASE_URL, {
-    ssl: true,
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   });
   // CREATE TABLE summoners(
   //   nickname VARCHAR(16) NOT NULL,
@@ -14,24 +17,30 @@ module.exports = function() {
   // );
 
   async function getUserDataByAmazonId(id) {
-    const summoners = await sql`select * from summoners WHERE amazonId = '${id}'`;
+    await client.connect()
+    const summoners = await client.query(`select * from summoners WHERE amazonId = '${id}'`).rows;
+    await client.end();
     return summoners.length > 0 ? summoners[0] : null;
   }
 
   async function saveUserDataByAmazonId(id, newData) {
-    await sql`insert into summoners (
-      nickname,
-      region,
-      puuid,
-      summonerId,
-      amazonId
-    ) values (
-      '${newData.nickname}',
-      '${newData.region}',
-      '${newData.puuid}',
-      '${newData.summonerId}',
-      '${id}'
-    )`;
+    await client.connect()
+    await client.query(
+      `insert into summoners (
+        nickname,
+        region,
+        puuid,
+        summonerId,
+        amazonId
+      ) values (
+        '${newData.nickname}',
+        '${newData.region}',
+        '${newData.puuid}',
+        '${newData.summonerId}',
+        '${id}'
+      )`
+    );
+    await client.end();
   }
 
   return {
